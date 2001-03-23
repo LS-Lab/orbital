2cb60b7f1efb10e600cf5f2424708f017790227f
Andre Platzer <aplatzer@cs.cmu.edu>
2001-03-23 16:06:05 +0000

    Initial revision

---
+++ src/orbital/util/DelegateSet.java
@@ -0,0 +1,43 @@
+/**
+ * @(#)DelegateSet.java 0.9 2000/09/05 Andre Platzer
+ *
+ * Copyright (c) 2000 Andre Platzer. All Rights Reserved.
+ */
+
+package orbital.util;
+
+import java.util.Set;
+
+import java.util.Collection;
+
+/**
+ * A DelegateSet that works as a delegator to sets.
+ * <p>
+ * This class simply implements Set and extends DelegateCollection, since
+ * a DelegateCollection can also be used as a delegate for Sets due
+ * to the identical interfaces differing only by semantics.</p>
+ *
+ * @version 0.9, 2000/09/05
+ * @author  Andr&eacute; Platzer
+ */
+public
+class DelegateSet/*<A>*/ extends DelegateCollection/*<A>*/ implements Set/*<A>*/ {
+	/**
+	 * Extend to create a Set delegating to an implementation Set.
+	 * @param delegatee the implementation-Set to that Set operations are delegated.
+	 */
+	protected DelegateSet(Set/*<A>*/ delegatee) {
+		super(delegatee);
+	}
+
+	protected void setDelegatee(Set/*<A>*/ delegatee) {
+		super.setDelegatee(delegatee);
+	}
+
+	protected void setDelegatee(Collection/*<A>*/ delegatee) {
+		if (delegatee instanceof Set/*<A>*/)
+			setDelegatee((Set/*<A>*/) delegatee);
+		else
+			throw new IllegalArgumentException("setDelegatee requires Set instance for DelegateSets");
+	}
+}
