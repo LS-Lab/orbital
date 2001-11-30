05a669b1d8a8c68fab62f4ffa58342cda0959be1
Andre Platzer <aplatzer@cs.cmu.edu>
2001-11-30 12:31:24 +0000

    This commit was manufactured by cvs2svn to create branch 'APlatzer'.

---
+++ src/_mklib.bat
@@ -0,0 +1,17 @@
+@ECHO Off
+pushd %1
+if exist exclude goto Fin
+dir %1\*.java /B >> %HOME%\java\orbital\class-list-new
+if exist substitute.bat goto substitute
+if not exist *.java goto Fin
+
+cd >> %HOME%\java\orbital\package-list-new
+cd
+call mklib %2 %3 %4 %5
+goto Fin
+
+ :substitute
+   rem substitue and discontinue further execution then since NO call is used
+    call substitute.bat
+ :Fin
+popd
