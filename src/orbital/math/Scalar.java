155ad80ca1d2d368b7a149adef67dfc6028f2f98
Andre Platzer <aplatzer@cs.cmu.edu>
2002-03-05 14:58:42 +0000

    This commit was manufactured by cvs2svn to create branch 'APlatzer'.

---
+++ src/orbital/math/Scalar.java
@@ -0,0 +1,36 @@
+/**
+ * @(#)Scalar.java 1.0 2000/08/08 Andre Platzer
+ * 
+ * Copyright (c) 2000 Andre Platzer. All Rights Reserved.
+ */
+
+package orbital.math;
+
+import orbital.logic.functor.Predicate;
+
+/**
+ * Abstraction of all scalar arithmetic number objects.
+ * <p>
+ * This class is the base class for all Arithmetic objects of scalar number type.
+ * Scalars usually implement {@link Comparable} as well.
+ * Most scalar implementation also tend to extend {@link java.lang.Number} but are not required
+ * to do so, since that would break the freedom of inheriting from another base class.
+ * </p>
+ * 
+ * @invariant usually this is Comparable || abnormal(Comparable)
+ * @version 1.0, 2000/08/08
+ * @author  Andr&eacute; Platzer
+ * @todo perhaps drop Comparable here?
+ */
+public
+interface Scalar extends Arithmetic/*, Comparable*/ {
+	/**
+	 * Checks whether the given arithmetic object is a number.
+	 * return whether v is complex, real, rational or an integer.
+	 */
+	public static final Predicate isa = new Predicate() {
+		public boolean apply(Object v) {
+    		return v instanceof Scalar;
+    	}
+	};
+}
