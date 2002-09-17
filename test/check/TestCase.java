d4979e50ac5563dc9d9f9f0c4a0b85a53175df50
Andre Platzer <aplatzer@cs.cmu.edu>
2002-09-17 16:26:34 +0000

    simple modification of junit.framework.TestCase for assertTrue to take parameters in the same order as assert does.

---
+++ test/check/TestCase.java
@@ -0,0 +1,27 @@
+/**
+ * @(#)TestCase.java 1.1 2002-09-14 Andre Platzer
+ *
+ * Copyright (c) 2002 Andre Platzer. All Rights Reserved.
+ */
+
+package check;
+
+/**
+ * TestCase.java
+ *
+ *
+ * @author <a href="mailto:">Andr&eacute; Platzer</a>
+ * @version 1.1, 2002-09-14
+ */
+public abstract class TestCase extends junit.framework.TestCase {
+    public TestCase() {
+	
+    }
+    public TestCase(String name) {
+	super(name);
+    }
+
+    public static void assertTrue(boolean condition, String message) {
+	junit.framework.TestCase.assertTrue(message, condition);
+    }
+}// TestCase
