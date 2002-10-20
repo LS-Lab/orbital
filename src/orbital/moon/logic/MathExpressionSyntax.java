8570a964288b29a84bfb6349891a75aeb88fef2b
Andre Platzer <aplatzer@cs.cmu.edu>
2002-10-20 15:57:00 +0000

    reintroduced &lambda;-abstractions like LogicParser.jj uses them.

--- src/orbital/moon/logic/MathExpressionSyntax.java
+++ src/orbital/moon/logic/MathExpressionSyntax.java
@@ -22,6 +22,8 @@ import orbital.logic.functor.Notation;
 
 import orbital.logic.trs.Substitutions;
 import java.lang.reflect.Field;
+import orbital.io.IOUtilities;
+import java.io.StringReader;
 
 /**
  * This class implements an expression syntax for mathematical expressions.
@@ -32,14 +34,68 @@ import java.lang.reflect.Field;
  * @author  Andr&eacute; Platzer
  * @structure depends orbital.logic.functor.Notation Uses registered notation functors reverse.
  */
-public class MathExpressionSyntax implements ExpressionBuilder {
+public class MathExpressionSyntax implements ExpressionSyntax {
     private static final Values valueFactory = Values.getDefaultInstance();
+    static final Symbol LAMBDA = ClassicalLogic.LAMBDA;
 	
+    /**
+     * tool-main
+     */
+    public static void main(String arg[]) throws Exception {
+	if (orbital.signe.isHelpRequest(arg)) {
+	    System.out.println(usage);
+	    return;
+	} 
+	System.out.print("Type expression: ");
+	System.out.flush();
+	String expr = IOUtilities.readLine(System.in);
+	Object p = new MathExpressionSyntax().createMathExpression(expr);
+	System.out.println("Original expression:\t" + expr);
+	System.out.println("Parsed function:\t" + p);
+	System.out.println("Evaluates to:\t" + (p instanceof Function ? ((Function) p).apply(null) : p));	//@XXX: erm why null?
+    } 
+    public static final String usage = "usage: \n\tstart and enter a correct mathematical expression to evaluate\n\tremember that function evaluation expressions are denoted with rectangular\n\tbrackets as in \"sin[2 + 4/15] + 3*x\"";
+
+
+    /**
+     * {@inheritDoc}.
+     * @see #createMathExpression(String)
+     */
+    public Expression createExpression(String expression) throws ParseException {
+	if (expression == null)
+	    throw new NullPointerException("null is not an expression");
+	try {
+	    MathParser parser = new MathParser(new StringReader(expression));
+	    parser.setSyntax(this);
+	    return parser.parseExpression();
+	} catch (orbital.moon.logic.ParseException ex) {
+	    throw new ParseException(ex.getMessage(), ex.currentToken.next.beginLine, ex.currentToken.next.beginColumn, ex);
+	} 
+    }
+    /**
+     * Parses an expression and queries its arithmetic object.
+     * @see #createExpression(String)
+     */
+    public Arithmetic createMathExpression(String expression) throws ParseException {
+	return (Arithmetic) getValueOf(createExpression(expression));
+    }
+
+    public Signature coreSignature() {
+	throw new UnsupportedOperationException("not yet implemented");
+    }
+    public Signature scanSignature(String expression) throws ParseException {
+	throw new UnsupportedOperationException("not yet implemented");
+    }
+
     public Expression createAtomic(Symbol symbol) {
 	Type cod = symbol.getType().codomain();
 	Type doc = symbol.getType().domain();
 	assert cod != Types.ABSURD : "@xxx currently assume map as strict";
-	if (cod.equals(Types.VOID))
+	// handle special cases of term construction, first
+	if (LAMBDA.equals(symbol))
+	    return new MathExpression(LAMBDA, LAMBDA.getType());
+
+	else if (cod.equals(Types.NOTYPE))
 	    if (doc.equals(Types.INDIVIDUAL))
 		return new MathExpression(valueFactory.symbol(symbol.getSignifier()), symbol.getType());
 	    else
@@ -64,10 +120,20 @@ public class MathExpressionSyntax implements ExpressionBuilder {
 	MathExpression op = (MathExpression)compositor;
 	if (!Types.isApplicableTo(compositor.getType(), arguments))
 	    throw new ParseException("compositor " + compositor + ":" + compositor.getType() + " not applicable to the " + arguments.length + " arguments " + MathUtilities.format(arguments) + ":" + Types.typeOf(arguments), ModernLogic.COMPLEX_ERROR_OFFSET);
+
+	// handle special cases of term construction, first
+	if (LAMBDA.equals(op.referee)) {
+	    assert arguments.length == 2;
+	    orbital.math.Symbol x = (orbital.math.Symbol) ((MathExpression)arguments[0]).referee;
+	    Object t = getValueOf(arguments[1]);
+	    return new MathExpression(convert((orbital.logic.functor.Function) Substitutions.lambda.apply(x, t)),
+				      Types.map(arguments[0].getType(), arguments[1].getType()));
+	}
+
 	MathFunctor f = (MathFunctor) op.getValue();
 	Object[] arg = new Object[arguments.length];
 	for (int i = 0; i < arg.length; i++)
-	    arg[i] = ((MathExpression)arguments[i]).getValue();
+	    arg[i] = getValueOf(arguments[i]);
 	return new MathExpression(apply(f, arg), op.getType().domain());
     }
 
@@ -124,6 +190,40 @@ public class MathExpressionSyntax implements ExpressionBuilder {
 	    throw new IllegalArgumentException("Calling a function '" + f  + "/" + arg.length + "' is not supported. Only 1 or two arguments can be used, yet");
 	}
     }
+
+    /**
+     * convert a orbital.logic.functor.Function to a orbital.math.functional.Function 
+     * that does not support other operations than apply.
+     */
+    private static final Function convert(final orbital.logic.functor.Function f) {
+	return new AbstractFunction() {
+		public Object apply(Object o) {
+		    return f.apply(o);
+		}
+		public Function derive() {
+		    throw new UnsupportedOperationException("not yet implemented, symbolic");
+		} 
+		public Function integrate() {
+		    throw new UnsupportedOperationException("not yet implemented, symbolic");
+		} 
+		public Real norm() {
+		    throw new UnsupportedOperationException();
+		}
+		public boolean equals(Object o) {
+		    return f.equals(o);
+		}
+		public int hashCode() {
+		    return f.hashCode();
+		}
+		public String toString() {
+		    return f.toString();
+		} 
+	    };
+    }
+
+    // identical to @see orbital.math.functional.Function (below)
+    /*private static*/ static abstract class AbstractFunction/*<A implements Arithmetic, B implements Arithmetic>*/  extends orbital.moon.math.AbstractFunctor implements Function/*<A,B>*/ {}
+
 
     private static class MathExpression implements Expression {
 	private Object referee;
