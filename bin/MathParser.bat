7294c331f1e2036c78932ec5e16d763b4fe630de
Andre Platzer <aplatzer@cs.cmu.edu>
2002-10-23 15:50:04 +0000

    adapted to conceptual change from MathParser to MathExpressionSyntax

--- bin/MathParser.bat
+++ bin/MathParser.bat
@@ -1,2 +1,2 @@
 @ECHO Off
-%JAVA_HOME%\bin\java.exe -classpath ..\lib\orbital-ext.jar;%CLASSPATH% orbital.moon.logic.MathParser %1 %2 %3 %4 %5
+%JAVA_HOME%\bin\java.exe -classpath ..\lib\orbital-ext.jar;%CLASSPATH% orbital.moon.logic.MathExpressionSyntax %1 %2 %3 %4 %5
