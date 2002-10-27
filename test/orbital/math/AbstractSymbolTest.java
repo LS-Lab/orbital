42e9e211ce3586335261e5e5f26a2a13b84fd139
Andre Platzer <aplatzer@cs.cmu.edu>
2002-10-27 12:38:43 +0000

    no message

---
+++ test/orbital/math/AbstractSymbolTest.java
@@ -0,0 +1,76 @@
+/**
+ * @(#)AbstractSymbolTest.java 1.1 2002-09-14 Andre Platzer
+ * 
+ * Copyright (c) 2002 Andre Platzer. All Rights Reserved.
+ */
+
+package orbital.math;
+
+import junit.framework.*;
+
+import orbital.moon.logic.MathExpressionSyntax;
+import orbital.logic.imp.ParseException;
+
+/**
+ * A sample test case, testing Values.
+ * @version 1.1, 2002-09-14
+ */
+public class AbstractSymbolTest extends check.TestCase {
+    private Values vf;
+    private MathExpressionSyntax syntax;
+
+    public static void main(String[] args) {
+	junit.textui.TestRunner.run(suite());
+    }
+    protected void setUp() {
+	vf = Values.getDefaultInstance();
+	// for parsing
+	syntax = new MathExpressionSyntax();
+    }
+    public static Test suite() {
+	return new TestSuite(AbstractSymbolTest.class);
+    }
+
+    public void testMatrixWithSymbols() {
+	Matrix M = vf.valueOf(new Arithmetic[][] {
+	    {vf.symbol("a"), vf.symbol("b")},
+	    {vf.symbol("c"), vf.symbol("d")}
+	});
+	Vector v = vf.valueOf(new Arithmetic[] {
+	    vf.valueOf(1), vf.valueOf(2)
+	});
+	
+	assertTrue( Values.symbolic.apply(M));
+	assertTrue(!Values.symbolic.apply(v));
+	assertTrue(!Values.symbolic.apply(vf.IDENTITY(7,7)));
+        assertTrue( Values.symbolic.apply(M.multiply(v)));
+
+	try {
+	    System.out.println(M + "*" + v + "=" + M.multiply(v));
+	    assertEquals(M.multiply(v), vf.valueOf(new Arithmetic[] {
+		syntax.createMathExpression("a+b*2"),
+		syntax.createMathExpression("c+d*2")
+	    }));
+
+	    System.out.println(M + "^-1 =\n" + M.inverse());
+
+	    M = vf.valueOf(new Arithmetic[][] {
+		{vf.valueOf(2), vf.symbol("a")},
+		{vf.symbol("d"), vf.valueOf(4)}
+	    });
+	    v = vf.valueOf(new Arithmetic[] {
+		vf.valueOf(1), vf.valueOf(2)
+	    });
+	    System.out.println(M + "*" + v + "=" + M.multiply(v));
+	    assertEquals(M.multiply(v), vf.valueOf(new Arithmetic[] {
+		syntax.createMathExpression("2+a*2"),
+		syntax.createMathExpression("d+8")
+	    }));
+	} catch (ParseException ex) {
+	    fail(ex.getMessage());
+	} catch (IllegalArgumentException ex) {
+	    fail(ex.getMessage());
+	}
+    } 
+
+}
---
+++ test/orbital/math/RMatrixTest.java
@@ -0,0 +1,61 @@
+/**
+ * @(#)RMatrixTest.java 1.1 2002-09-14 Andre Platzer
+ * 
+ * Copyright (c) 2002 Andre Platzer. All Rights Reserved.
+ */
+
+package orbital.math;
+
+import junit.framework.*;
+
+/**
+ * A sample test case, testing Values.
+ * @version 1.1, 2002-09-14
+ */
+public class RMatrixTest extends check.TestCase {
+    private Values vf;
+
+    public static void main(String[] args) {
+	junit.textui.TestRunner.run(suite());
+    }
+    protected void setUp() {
+	vf = Values.getDefaultInstance();
+    }
+    public static Test suite() {
+	return new TestSuite(RMatrixTest.class);
+    }
+
+    public void testRMatrixAndFallback() {
+	final Values vf = Values.getDefaultInstance();
+	Matrix M = new RMatrix(new double[][] {
+	    {2, 1, 0, -2},
+	    {1, 2, 4, 1},
+	    {-2, 1, 2, -2},
+	    {-3, 0, 1, -4}
+	});
+	Vector v = new RVector(new double[] {
+	    1, 2, 1, 2
+	});
+	Vector u = new RVector(new double[] {
+	    2, 1, 0, -3
+	});
+	System.out.println(M + "*" + v + "=" + M.multiply(v));
+	assertEquals(M.multiply(v), vf.valueOf(new double[] {
+	    0, 11, -2, -10
+	}));
+	System.out.println(u + "*" + v + "=" + u.multiply(v));
+	assertEquals(u.multiply(v),vf.valueOf(-2));
+	System.out.println(v + "*" + 2 + "=" + v.multiply(vf.valueOf(2)));
+	assertEquals(u.multiply(vf.valueOf(2)),vf.valueOf(new double[] {
+	    4, 2, 0, -6
+	}));
+	System.out.println("M^-1=" + M.inverse());
+	System.out.println("fall-back to arithmetic matrix");
+	v = vf.valueOf(new Arithmetic[] {
+	    vf.valueOf(1), vf.complex(3,-4), vf.rational(-1, 3), vf.rational(1, 2)
+	});
+	System.out.println(M + "*" + v + "=" + M.multiply(v));
+	assertTrue(!((Complex)M.multiply(v).get(0)).im().equals(Values.ZERO));
+    } 
+
+}
