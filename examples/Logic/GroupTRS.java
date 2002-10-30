bd96844f42978f527c756678ced564d0f0e82ec7
Andre Platzer <aplatzer@cs.cmu.edu>
2002-10-30 15:32:11 +0000

    no message

--- examples/Logic/GroupTRS.java
+++ examples/Logic/GroupTRS.java
@@ -1,9 +1,12 @@
 import orbital.logic.functor.*;
 import orbital.moon.logic.MathExpressionSyntax;
+import orbital.moon.logic.LogicParser;
 import orbital.logic.trs.*;
+import orbital.logic.imp.Expression;
 import orbital.logic.imp.ParseException;
 import orbital.io.IOUtilities;
 import java.util.*;
+import java.io.*;
 
 /**
  * Group Term Rewrite System (TRS).
@@ -16,6 +19,16 @@ import java.util.*;
  */
 public class GroupTRS {
     public static void main(String arg[]) throws Exception {
+	new GroupTRS().run();
+    }
+    /**
+     * the syntax of mathematical expressions used for parsing.
+     */
+    private MathExpressionSyntax syntax;
+    public GroupTRS() {
+	this.syntax = new MathExpressionSyntax();
+    }
+    public void run() throws Exception {
 	System.out.print("Type first expression (A): ");
 	System.out.flush();
 	String expr = IOUtilities.readLine(System.in);
@@ -29,42 +42,37 @@ public class GroupTRS {
 	System.out.println("Original expression:\t" + expr);
 	System.out.println("Parsed function:\t" + B);
 		
-	// apply a nice substitution
-	Substitution sigma = Substitutions.getInstance(Arrays.asList(new Object[] {
-	    // axiom left identity
-	    Substitutions.createSingleSidedMatcher(parse("1*_X"), parse("_X")),
-	    // axiom left inverse
-	    Substitutions.createSingleSidedMatcher(parse("_X^-1*_X"), parse("1")),
-	    // axiom associative
-	    Substitutions.createSingleSidedMatcher(parse("(_X1*_X2)*_X3"), parse("_X1*(_X2*_X3)")),
-	    //
-	    Substitutions.createSingleSidedMatcher(parse("_X1^-1*(_X1*_X2)"), parse("_X2")),
-	    // identity is idem potent
-	    Substitutions.createSingleSidedMatcher(parse("1^-1"), parse("1")),
-	    // right identity
-	    Substitutions.createSingleSidedMatcher(parse("_X*1"), parse("_X")),
-	    // inversion is involutive
-	    Substitutions.createSingleSidedMatcher(parse("(_X^-1)^-1"), parse("_X")),
-	    // right inverse
-	    Substitutions.createSingleSidedMatcher(parse("_X*_X^-1"), parse("1")),
-	    //
-	    Substitutions.createSingleSidedMatcher(parse("_X1*((_X1^-1)*_X8)"), parse("_X8")),
-	    // inverse of a product
-	    Substitutions.createSingleSidedMatcher(parse("(_X1*_X2)^-1"), parse("_X2^-1*_X1^-1")),
-	}));
-		
+	// load a nice substitution to apply, from a file
+	Substitution sigma = LogicParser.readTRS(
+	    new InputStreamReader(getClass().getResourceAsStream("group.trs")),
+	    syntax,
+	    unwrapArithmeticObjects
+	    );
+
+	// apply the substitution
 	System.out.println("applying " + sigma);
+	System.out.println("is");
 	System.out.println("sigma(A) = " + sigma.apply(A));
 	System.out.println("sigma^2(A) = " + sigma.apply(sigma.apply(A)));
 	System.out.println("sigma^3(A) = " + sigma.apply(sigma.apply(sigma.apply(A))));
 	Object A_ = Functionals.fixedPoint(sigma, A);
-	Object B_ = Functionals.fixedPoint(sigma, B);
 	System.out.println("TRS:\t\t\tA->" + A_);
+	Object B_ = Functionals.fixedPoint(sigma, B);
 	System.out.println("TRS:\t\t\tB->" + B_);
 	System.out.println(A.equals(B) ? "A=B" : "A!=B");
     } 
 	
-    private static final Object parse(String expression) throws ParseException {
-	return new MathExpressionSyntax().createMathExpression(expression);
+    private final Object parse(String expression) throws ParseException {
+	return syntax.createMathExpression(expression);
     }
+
+    /**
+     * Transformation that unwraps the represented arithmetic object
+     * of a parsed expression.
+     */
+    private final Function unwrapArithmeticObjects = new Function() {
+	    public Object apply(Object x) {
+		return syntax.getValueOf((Expression)x);
+	    }
+	};
 }
