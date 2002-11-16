9c345b3f1b61842e7fe5ed42e7ca578d0e1ab024
Andre Platzer <aplatzer@cs.cmu.edu>
2002-11-16 15:52:17 +0000

    use AlgorithmicConfigurations

--- test/orbital/algorithm/template/SearchTest.java
+++ test/orbital/algorithm/template/SearchTest.java
@@ -7,14 +7,17 @@
 import junit.framework.*;
 
 import orbital.algorithm.template.*;
+import orbital.algorithm.template.AlgorithmicTemplate.Configuration;
 import orbital.logic.functor.Function;
 import orbital.math.functional.Functions;
 import orbital.math.Scalar;
 import orbital.math.Values;
 import orbital.math.Symbol;
+import orbital.math.Real;
 import orbital.awt.*;
 import java.lang.reflect.*;
 import java.util.*;
+import orbital.util.Setops;
 
 import java.util.logging.Logger;
 import java.util.logging.Level;
@@ -30,130 +33,178 @@ import java.util.logging.Level;
  */
 public class SearchTest extends check.TestCase {
     private static final Logger logger = Logger.global;
-    private static final int TEST_REPETITION = 2;
+    private static final int TEST_REPETITION = 1;
     private static final int SIMPLE_GSP_RANGE = 5;
     private static final int UNSOLVABLE_GSP_RANGE = 4;
-    // pseudo-heuristic to be replaced lateron
-    private static final Function h = Functions.one;
-    private static final Function schedule = new Function() {
-	    public Object apply(Object o) {
-		int i = ((Number) o).intValue();
-		double d = 10 - i/175.;
-		return new Double(Math.max(d, 0));
-	    }
-	};
-    public static final AlgorithmicTemplate[] defaultAlgo = {
-	/*"DepthFirstSearch", */ new BreadthFirstSearch(), new IterativeDeepening(), /*"IterativeBroadening", */new AStar(h), new HillClimbing(h), new IterativeDeepeningAStar(h), new BranchAndBound(h, 0), new ParallelBranchAndBound(h, 0),
-	new IterativeExpansion(h),
-	new SimulatedAnnealing(h, schedule), new ThresholdAccepting(h, schedule),
-	new WAStar(2, h)
-    };
-    /**
-     * Application-start entry point.
-     */
-    public static void main(String arg[]) throws Exception {
-	new SearchTest().test(defaultAlgo);
+
+    private AlgorithmicConfiguration[] defaultAlgo;
+    private Random random;
+    public static void main(String[] args) {
+	junit.textui.TestRunner.run(suite());
     }
     public static Test suite() {
 	return new TestSuite(SearchTest.class);
     }
+
+    protected void setUp() {
+	this.random = new Random();
+	final Real maxBound = Values.getDefaultInstance().valueOf(2*SIMPLE_GSP_RANGE+SimpleGSP.PAY_FOR_PASSING);
+	final Function h = SimpleGSP.createHeuristic();
+	final Function schedule = new Function() {
+		public Object apply(Object o) {
+		    int i = ((Number) o).intValue();
+		    double d = 10 - i/175.;
+		    return new Double(Math.max(d, 0));
+		}
+	    };
+	try {
+	    //@internal the problem is ignored by test(...) anyway, so let's temporarily use null.
+	    this.defaultAlgo = new AlgorithmicConfiguration[] {
+		/*"DepthFirstSearch", */
+		new Configuration(null, BreadthFirstSearch.class),
+		new Configuration(null, IterativeDeepening.class),
+		/*"IterativeBroadening", */
+		new HeuristicAlgorithm.Configuration(null, h, AStar.class),
+		new HeuristicAlgorithm.Configuration(null, h, HillClimbing.class),
+		new HeuristicAlgorithm.Configuration(null, h, IterativeDeepeningAStar.class),
+		Configuration.flexible(null, Setops.asMap(new Object[][] {
+		    {"heuristic", h},
+		    {"maxBound", maxBound}
+		}), BranchAndBound.class),
+		Configuration.flexible(null, Setops.asMap(new Object[][] {
+		    {"heuristic", h},
+		    {"maxBound", maxBound}
+		}), ParallelBranchAndBound.class),
+		new HeuristicAlgorithm.Configuration(null, h, IterativeExpansion.class),
+		Configuration.flexible(null, Setops.asMap(new Object[][] {
+		    {"heuristic", h},
+		    {"schedule", schedule}
+		}), SimulatedAnnealing.class),
+		Configuration.flexible(null, Setops.asMap(new Object[][] {
+		    {"heuristic", h},
+		    {"schedule", schedule}
+		}), ThresholdAccepting.class),
+		Configuration.flexible(null, Setops.asMap(new Object[][] {
+		  {"heuristic", h},
+		  {"weight", Values.getDefaultInstance().valueOf(2)}
+		  }), WAStar.class)
+	    };
+	} catch (java.beans.IntrospectionException ex) {
+	    ex.printStackTrace();
+	    fail(ex.getMessage());
+	}
+    }
     
-    private static final Random random = new Random();
-    
-    public void testAllSearchAlgorithms() {
+    public void test() {
 	test(defaultAlgo);
     }
 
-    // individual tests for timing
-    public void testBreadthFirstSearch() {
-	test(new BreadthFirstSearch());
-    }
     
-    public void testIterativeDeepening() {
-	test(new IterativeDeepening());
-    }
-    public void testAStar() {
-	test(new AStar(h));
-    }
-    public void testHillClimbing() {
-	test(new HillClimbing(h));
-    }
-    public void testIterativeDeepeningAStar() {
-	test(new IterativeDeepeningAStar(h));
-    }
-    public void testBranchAndBound() {
-	test(new BranchAndBound(h, 0));
-    }
-    public void testParallelBranchAndBound() {
-	test(new ParallelBranchAndBound(h, 0));
-    }
-    public void testIterativeExpansion() {
-	test(new IterativeExpansion(h));
-    }
-    public void testSimulatedAnnealing() {
-	test(new SimulatedAnnealing(h, schedule));
-    }
-    public void testThresholdAccepting() {
-	test(new ThresholdAccepting(h, schedule));
-    }
-    public void testWAStar() {
-	test(new WAStar(2, h));
-    }
-    
-    
-    protected void test(AlgorithmicTemplate algo[]) {
-	for (int i = 0; i < algo.length; i++)
+    protected void test(AlgorithmicConfiguration algo[]) {
+	System.out.println("Testing " + algo.length + " algorithms ...");
+	for (int i = 0; i < algo.length; i++) {
+	    System.out.println("Testing No. " + i + " : " + algo[i]);
 	    test(algo[i]);
+	}
     }
 
-    protected void test(AlgorithmicTemplate algo) {
+    protected void test(AlgorithmicConfiguration config) {
+	try {
+	    AlgorithmicTemplate algo = config.getAlgorithm();
+	    System.out.println("Testing " + algo);
+
+	    // instantiate
+	    boolean complete = false;
+	    boolean correct = false;
+	    boolean optimal;
 	    try {
-		// instantiate
-		boolean complete = false;
-		boolean correct = false;
-		boolean optimal;
-		try {
-		    complete = (algo.complexity() != Functions.nondet 
-				//@xxx: norm is infinite for all polynoms, what else!
-				&& !(algo.complexity().equals(Functions.constant(Values.getDefaultInstance().valueOf(Double.POSITIVE_INFINITY)))));
-		}
-		catch(UnsupportedOperationException x) {logger.log(Level.INFO, "unsupported", x);}
-		try {
-		    correct = algo instanceof ProbabilisticAlgorithm ? ((ProbabilisticAlgorithm) algo).isCorrect() : true;
-		}
-		catch(UnsupportedOperationException x) {logger.log(Level.INFO, "unsupported", x);}
-		optimal = algo instanceof GeneralSearch ? ((GeneralSearch) algo).isOptimal() : false;
-		// special handling
-		if (algo instanceof HeuristicAlgorithm)
-		    ((HeuristicAlgorithm)algo).setHeuristic(SimpleGSP.createHeuristic());
-		if (algo instanceof BranchAndBound)
-		    ((BranchAndBound)algo).setMaxBound(2*SIMPLE_GSP_RANGE+SimpleGSP.PAY_FOR_PASSING);
-				
+		complete = (algo.complexity() != Functions.nondet 
+			    //@xxx: norm is infinite for all polynoms, what else!
+			    && !(algo.complexity().equals(Functions.constant(Values.getDefaultInstance().valueOf(Double.POSITIVE_INFINITY)))));
+	    }
+	    catch(UnsupportedOperationException x) {logger.log(Level.INFO, "unsupported", x);}
+	    try {
+		correct = algo instanceof ProbabilisticAlgorithm ? ((ProbabilisticAlgorithm) algo).isCorrect() : true;
+	    }
+	    catch(UnsupportedOperationException x) {logger.log(Level.INFO, "unsupported", x);}
+	    optimal = algo instanceof GeneralSearch ? ((GeneralSearch) algo).isOptimal() : false;
 
-		System.out.println("Testing " + algo);
-
-		// test problems that have a solution
-		for (int rep = 0; rep < TEST_REPETITION; rep++) {
-		    VerifyingSimpleGSP p = new VerifyingSimpleGSP((int)(random.nextFloat()*2*SIMPLE_GSP_RANGE-SIMPLE_GSP_RANGE), (int)(random.nextFloat()*2*SIMPLE_GSP_RANGE-SIMPLE_GSP_RANGE));
-		    System.out.println("Test solvable problem " + p + " for " + algo);
-		    Object solution = algo.solve(p);
-		    assertTrue( solution != null , algo + " should solve a problem that admits a solution. " + p);
-		    if (!p.isSolution(solution))
-			System.out.println(algo + " did not solve a problem that admits a solution. " + p + " \"solution\" found " + solution);
-		    if (optimal)
-			assertTrue(p.isOptimalSolution(solution) , algo + " is optimal but has found only a suboptimal solution " + solution + " to " + p);
-		}
+	    // test problems that have a solution
+	    for (int rep = 0; rep < TEST_REPETITION; rep++) {
+		VerifyingSimpleGSP p = new VerifyingSimpleGSP((int)(random.nextFloat()*2*SIMPLE_GSP_RANGE-SIMPLE_GSP_RANGE), (int)(random.nextFloat()*2*SIMPLE_GSP_RANGE-SIMPLE_GSP_RANGE));
+		System.out.println("Test solvable problem " + p + " for " + algo);
+		Object solution = algo.solve(p);
+		assertTrue( solution != null , algo + " should solve a problem that admits a solution. " + p);
+		if (!p.isSolution(solution))
+		    System.out.println(algo + " did not solve a problem that admits a solution. " + p + " \"solution\" found " + solution);
+		if (optimal)
+		    assertTrue(p.isOptimalSolution(solution) , algo + " is optimal but has found only a suboptimal solution " + solution + " to " + p);
+	    }
 				
-		// test problems that do not have a solution (but a finite search space)
-		for (int rep = 0; rep < TEST_REPETITION; rep++) {
-		    UnsolvableSimpleGSP p = new UnsolvableSimpleGSP((int)(random.nextFloat()*UNSOLVABLE_GSP_RANGE));
-		    System.out.println("Test unsolvable problem " + p + " for " + algo);
-		    Object solution = algo.solve(p);
-		    if (correct)
-			assertTrue(solution == null , algo + " should not \"solve\" a problem that has no solution. " + p + " \"solution\" " + solution);
-		}
-	    } catch (Exception ignore) {
-		logger.log(Level.FINER, "introspection", ignore);
-	    } 
+	    // test problems that do not have a solution (but a finite search space)
+	    for (int rep = 0; rep < TEST_REPETITION; rep++) {
+		UnsolvableSimpleGSP p = new UnsolvableSimpleGSP((int)(random.nextFloat()*UNSOLVABLE_GSP_RANGE));
+		System.out.println("Test unsolvable problem " + p + " for " + algo);
+		Object solution = algo.solve(p);
+		if (correct)
+		    assertTrue(solution == null , algo + " should not \"solve\" a problem that has no solution. " + p + " \"solution\" " + solution);
+	    }
+	} catch (Exception ignore) {
+	    logger.log(Level.FINER, "introspection", ignore);
+	    ignore.printStackTrace();
+	    fail(ignore.getMessage());
+	} 
     } 
+
+
+    // individual tests for timing
+    public void test0() {
+	test(0);
+    }
+    public void test1() {
+	test(1);
+    }
+    public void test2() {
+	test(2);
+    }
+    public void test3() {
+	test(3);
+    }
+    public void test4() {
+	test(4);
+    }
+    public void test5() {
+	test(5);
+    }
+    public void test6() {
+	test(6);
+    }
+    public void test7() {
+	test(7);
+    }
+    public void test8() {
+	test(8);
+    }
+    public void test9() {
+	test(9);
+    }
+    public void test10() {
+	test(10);
+    }
+    public void test11() {
+	test(11);
+    }
+    public void test12() {
+	test(12);
+    }
+    public void test13() {
+	test(13);
+    }
+    public void test14() {
+	test(14);
+    }
+    private void test(int index) {
+	if (index < defaultAlgo.length)
+	    test(defaultAlgo[index]);
+    }
 }
