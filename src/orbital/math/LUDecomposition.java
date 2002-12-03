a558b4bc426c48d86d04759fc7910939b8551878
Andre Platzer <aplatzer@cs.cmu.edu>
2002-12-03 19:35:10 +0000

    fixed assertion that needs tolerant equality for machine precision rounding problems.

--- src/orbital/math/LUDecomposition.java
+++ src/orbital/math/LUDecomposition.java
@@ -103,7 +103,7 @@ public final class LUDecomposition/*<R implements Arithmetic>*/ implements Seria
 	} 
 	this.A = A;
 	this.P = P;
-	assert P.multiply(M).equals(getL().multiply(getU())) : "P.A = L.U: " + P + "*" + M + "=" + P.multiply(M) + " = " + getL().multiply(getU()) + "=" + getL() + "*" + getU();
+	assert P.multiply(M).equals(getL().multiply(getU()), Values.getDefaultInstance().valueOf(MathUtilities.getDefaultTolerance())) : "P.A = L.U: " + P + "*" + M + "=" + P.multiply(M) + "  =  " + getL().multiply(getU()) + "=" + getL() + "*" + getU();
 
 	/*
 	  Alternative implementation:
