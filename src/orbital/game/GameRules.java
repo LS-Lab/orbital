020b8609e634d37adbab4336a89cff07453a1836
Andre Platzer <aplatzer@cs.cmu.edu>
2003-01-05 09:09:28 +0000

    Adapted to introduced FieldChangeEvents notifying listeners of changes on the field. Removed old explicit notification call methods and replaced by documentation of the new event mechanism.

--- src/orbital/game/GameRules.java
+++ src/orbital/game/GameRules.java
@@ -12,9 +12,29 @@ import java.awt.Image;
 
 /**
  * An interface defining the behaviour of a generic game with a set of rules.
- * 
+ * <p>
+ * Implementations have to {@link Field#getFieldChangeMulticaster() post}
+ * events to the <code>Field</code> they started, at each end of
+ * turn and at the end of the game. In order to post such events,
+ * implementations are encouraged to register themselves to the field
+ * they started and listen for <code>FieldChangeEvent.MOVE</code>
+ * events, as well.  However, when control flow permits,
+ * implementations could also place end of game checks and end of turn
+ * checks within, for example, there {@link
+ * FigureImpl#moving(Move,Position)} implementation. However, those
+ * events will then also be generated (in vain) in lookup scenarios as
+ * occurring in AdversarySearch.
+ * </p>
+ *
+ * @version 1.1, 2003-01-04
  * @version 0.9, 2000/02/26
  * @author  Andr&eacute; Platzer
+ * @events FieldChangeEvent.END_OF_TURN(league) at the end of the turn of the specified league.
+ * @events FieldChangeEvent.END_OF_GAME(winner) at the end of the game.
+ *  Thereby, winner is a value whose absolute specifies the league that has won.
+ *  A positive number says that the specified player has reached the goal of the game.
+ *  The value is allowed to be negative in which case it indicates that
+ *  the winner, instead of actively reaching the goal of the game, only won because all others have lost.
  */
 public interface GameRules {
 
@@ -30,10 +50,10 @@ public interface GameRules {
      * Constructs a new AI Computer-Opponent for the Game.
      * @param arg can contain any value used to initialize the AI, including <code>null</code>.
      * @return a function that, when called with a situation state as its argument,
-     *  will return the action it wants to take in that state. (See {@link MoveWeighting.Argument}).
+     *  will return the action it wants to take in that state. (encoded as a {@link AdversarySearch.Option}).
      * @see <a href="{@docRoot}/Patterns/Design/FactoryMethod.html">Factory Method</a>
      */
-    Function startAIntelligence(String arg);
+    Function/*<Field,AdversarySearch.Option>*/ startAIntelligence(String arg);
 
     /**
      * Get the image-object to be used for the given figure.
@@ -62,14 +82,4 @@ public interface GameRules {
      */
     int getFigureTypes();
 
-    /**
-     * Is called after each turn to check who won.
-     * @param field the field the game is being played on to check who won the game.
-     * @return a value whose absolute specifies the league that has won, {@link Figure#NOONE} if there is no winner yet.
-     *  A positive number says that the specified player has reached the goal of the game.
-     *  {@link Figure#NOONE} says that no one has won yet.
-     *  The value is allowed to be negative in which case it indicates that
-     *  the winner, instead of actively reaching the goal of the game, only won because all others have lost.
-     */
-    int turnDone(Field field);
 }
