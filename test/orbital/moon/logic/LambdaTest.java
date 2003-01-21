00e13b618b979e51acb7fb12a0afb4c6843b5d4c
Andre Platzer <aplatzer@cs.cmu.edu>
2003-01-21 19:05:22 +0000

    introduce test for (some simple) lambda expressions in classical logic.

---
+++ test/orbital/moon/logic/LambdaTest.java
@@ -0,0 +1,48 @@
+/**
+ * @(#)LambdaTest.java 1.1 2002-11-24 Andre Platzer
+ * 
+ * Copyright (c) 2002 Andre Platzer. All Rights Reserved.
+ */
+
+package orbital.moon.logic;
+
+import junit.framework.*;
+import java.io.*;
+
+/**
+ * A sample test case, testing ClassicalLogic.
+ * @version 1.1, 2002-11-24
+ */
+public class LambdaTest extends check.TestCase {
+    private static final String ENCODING = "utf-8";
+
+    public static void main(String[] args) {
+	junit.textui.TestRunner.run(suite());
+    }
+    public static Test suite() {
+	return new TestSuite(LambdaTest.class);
+    }
+    protected void setUp() {
+    }
+
+    protected void test(String name, boolean allTrue) {
+	try {
+	    ClassicalLogic logic = new ClassicalLogic();
+	    ClassicalLogic.proveAll(new InputStreamReader(this.getClass().getResourceAsStream(name),
+							  ENCODING),
+				    logic, allTrue);
+	}
+	catch (Throwable ex) {
+	    ex.printStackTrace();
+	    fail(ex.getMessage() + " in file " + name);
+	}
+    }
+    public void testEquivalences() {
+	test("lambda-equivalence.txt", true);
+    }
+    public void testGarbage() {
+	test("lambda-garbage.txt", false);
+    }
+}
+
+
