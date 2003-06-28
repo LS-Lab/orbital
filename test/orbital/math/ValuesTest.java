8246f535386f6f16a743428cff0308bb33252bf0
Andre Platzer <aplatzer@cs.cmu.edu>
2003-06-28 18:03:22 +0000

    introduce testing tiny tensors of one component, only.

--- test/orbital/math/ValuesTest.java
+++ test/orbital/math/ValuesTest.java
@@ -7,15 +7,18 @@
 package orbital.math;
 
 import junit.framework.*;
+import java.util.*;
 
 /**
  * A sample test case, testing Values.
  * @version 1.1, 2002-09-14
  */
 public class ValuesTest extends check.TestCase {
+    private static final int TEST_REPETITION = 10;
     private Values vf;
     private Arithmetic a[];
     private Arithmetic b[];
+    private Random random;
 
     public static void main(String[] args) {
 	junit.textui.TestRunner.run(suite());
@@ -31,6 +34,7 @@ public class ValuesTest extends check.TestCase {
 	b = new Arithmetic[] {
 	    vf.valueOf(7), vf.rational(3, 4), vf.valueOf(3.1415926), vf.complex(3, 2)
 	};
+	random = new Random();
     }
 
     public void testInteroperableTypeAdd() {
@@ -49,4 +53,61 @@ public class ValuesTest extends check.TestCase {
 	    assertTrue( x1.getClass() == x2.getClass(), "compile-time sub-type result equals run-time sub-type");
 	}
     }
+
+    private int integerArgument(int min, int max) {
+	return min + (min == max ? 0 : random.nextInt(max-min));
+    }
+    public void testTrivialTensors_valueOf_ArithmeticArray() {
+	System.out.println("ValueFactor.valueOf(Arithmetic[])");
+	for (int i = 0; i < TEST_REPETITION; i++) {
+	    Integer v = vf.valueOf(integerArgument(-10,10));
+	    test(vf.valueOf(new Arithmetic[] {v}), v);
+	}
+    }
+    public void testTrivialTensors_valueOf_intArray() {
+	System.out.println("ValueFactor.valueOf(int[])");
+	for (int i = 0; i < TEST_REPETITION; i++) {
+	    Integer v = vf.valueOf(integerArgument(-10,10));
+	    test(vf.valueOf(new int[] {v.intValue()}), v);
+	}
+    }
+    public void testTrivialTensors_tensor_ArithmeticArray() {
+	System.out.println("ValueFactor.tensor(Arithmetic[])");
+	for (int i = 0; i < TEST_REPETITION; i++) {
+	    Integer v = vf.valueOf(integerArgument(-10,10));
+	    test(vf.tensor(new Arithmetic[] {v}), v);
+	}
+    }
+    public void testTrivialTensors_tensor_intArray() {
+	System.out.println("ValueFactor.tensor(int[])");
+	for (int i = 0; i < TEST_REPETITION; i++) {
+	    Integer v = vf.valueOf(integerArgument(-10,10));
+	    test(vf.tensor(new int[] {v.intValue()}), v);
+	}
+    }
+
+    /**
+     * Test tensor to have expected value as its only component.
+     */
+    private void test(Tensor t, Arithmetic expected) {
+	System.out.println(t.getClass() + " " + t + " corresponds to " + expected);
+	assertEquals(t.rank(), 1);
+	assertEquals(t.dimensions().length, 1);
+	assertEquals(t.dimensions()[0], 1);
+	assertEquals(t.get(new int[] {0}), expected);
+	Iterator i = t.iterator();
+	assertTrue(i.hasNext() , "tensor with 1 element has elements in its iterator");
+	assertEquals(i.next(), expected);
+	assertTrue(!i.hasNext() , "tensor with 1 element has no more than 1elements in its iterator");
+
+	assertTrue(t instanceof Vector, "rank 1 tensor collapses to vector");
+	Vector v = (Vector) t;
+	assertEquals(v.dimension(), 1);
+	assertEquals(v.get(0), expected);
+	i = v.iterator();
+	assertTrue(i.hasNext() , "tensor with 1 element has elements in its iterator");
+	assertEquals(i.next(), expected);
+	assertTrue(!i.hasNext() , "tensor with 1 element has no more than 1elements in its iterator");
+    }
 }
+
