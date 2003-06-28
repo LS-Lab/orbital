6de1cd57b74dc530e0fa2cbf73bfb507d112e1c8
Andre Platzer <aplatzer@cs.cmu.edu>
2003-06-28 17:57:08 +0000

    fixed previous() operation and false mark finish operation. Both have been causing serious trouble trhoughout orbital.algorithm.Combinatorical, orbital.math.Tensor, orbital.math.ValueFactory, orbital.algorithm.Combinatorical.getPermutations(int[]), orbital.math.MathUtilities#format(Object), orbital.util.Utility.asIteratable and thus also the tests of other Combinatorical.get* algorithms. Caused the cannot do 1 of n bugs, and the bugs with tensors/vectors containing only one element.

--- src/orbital/algorithm/GeneralizedPermutation.java
+++ src/orbital/algorithm/GeneralizedPermutation.java
@@ -15,29 +15,32 @@ import orbital.util.SuspiciousError;
 import orbital.math.functional.Operations;
 import orbital.math.Values;
 import orbital.math.Integer;
+import orbital.util.Utility;
 
 /**
  * Generalized Per[n] with n&isin;<b>N</b><sup>r</sup>.
  * @version 0.9, 2002-08-18
  * @internal (apart from more flexible bounds, dimensions instead of n, almost) identical to @see RepetitivePermutation
+ * @todo why do we start changing index.length - 1 instead of 0?
  */
 class GeneralizedPermutation extends Combinatorical {
     private static final long serialVersionUID = 6710387882794688842L;
-    private int[] dimensions;
+    private final int[] dimensions;
     private int[] index;
     /**
      * Create a new multi index iterator starting at (0,...,0).
      */
-    public GeneralizedPermutation(int[] dimensions) {
+    public GeneralizedPermutation(final int[] dimensions) {
 	this.dimensions = dimensions;
-	this.index = new int[dimensions.length];
-	Arrays.fill(index, 0);
+	this.index = zero();
     }
 
     public boolean hasNext() {
-	for (int k = index.length - 1; k >= 0; k--)
-	    if (index[k] >= dimensions[k])
+	for (int k = index.length - 1; k >= 0; k--) {
+	    if (index[k] >= dimensions[k]) {
 		return false;
+	    }
+	}
 	return true;
     } 
 
@@ -46,25 +49,37 @@ class GeneralizedPermutation extends Combinatorical {
 	    throw new NoSuchElementException();
 	int[] r = (int[]) index.clone();
 	for (int k = index.length - 1; k >= 0; k--) {
-	    if (++index[k] == dimensions[k])
+	    index[k]++;
+	    if (index[k] == dimensions[k]) {
 		index[k] = 0;
-	    else if (index[k] > dimensions[k]) {
+	    } else if (index[k] > dimensions[k]) {
 		assert false : NoSuchElementException.class + " should already have occurred";
-	    } else
+	    } else if (index[k] < dimensions[k]) {
 		return r;
+	    } else {
+		assert false;
+	    }
 	}
-	if (index.length == 0)
+	// went through all possibilities, finish of iterator
+	if (index.length == 0) {
 	    return r;
-	else
+	} else {
+	    assert Utility.equalsAll(index, zero()) : "rotated back to 0";
 	    // mark finish
-	    index[0] = dimensions[0] + 1;
+	    for (int i = 0; i < index.length; i++) {
+		index[i] = dimensions[i] - 1;
+	    }
+	    index[index.length - 1]++;
+	}
 	return r;
-    } 
+    }
 
     public boolean hasPrevious() {
-	for (int k = index.length - 1; k >= 0; k--)
-	    if (index[k] - 1 >= 0)
+	for (int k = index.length - 1; k >= 0; k--) {
+	    if (index[k] - 1 >= 0) {
 		return true;
+	    }
+	}
 	return false;
     } 
 
@@ -72,15 +87,17 @@ class GeneralizedPermutation extends Combinatorical {
 	if (!hasPrevious())
 	    throw new NoSuchElementException();
 	for (int k = index.length - 1; k >= 0; k--) {
-	    if (--index[k] < 0)
+	    if (--index[k] < 0) {
 		index[k] = dimensions[k] - 1;
-	    else
+	    } else {
 		return index;
+	    }
 	} 
-	if (index.length == 0)
+	if (index.length == 0) {
 	    return index;
-	else
+	} else {
 	    throw new SuspiciousError();
+	}
     } 
 
     public int count() {
@@ -89,5 +106,14 @@ class GeneralizedPermutation extends Combinatorical {
 
     public String toString() {
 	return getClass().getName() + "[of " + MathUtilities.format(dimensions) + " elements]";
+    }
+
+    /**
+     * 0 = (0,...,0)
+     */
+    private int[] zero() {
+	int[] zero = new int[dimensions.length];
+	Arrays.fill(zero, 0);
+	return zero;
     }
 }
