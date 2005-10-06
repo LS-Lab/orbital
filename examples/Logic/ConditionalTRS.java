aa814c3e16cb16380189288187fedb38a6ac744f
Andre Platzer <aplatzer@cs.cmu.edu>
2005-10-06 14:18:19 +0000

    Improve example.

--- examples/Logic/ConditionalTRS.java
+++ examples/Logic/ConditionalTRS.java
@@ -11,16 +11,23 @@ import java.util.Arrays;
 
 /**
  * Conditional Term Rewrite System (TRS).
- * It will parse two expressions and transform them according to
- * a rewrite system with
- * _X1&_X2 |- _X2  /;  when _X1 can be simplified to true
+ * It will parse a formula and transform it according to a
+ * rewrite system with--amongst others--the following conditional
+ * rewrite rule
+ * <pre>_X1&_X2 |- _X2  /;  when _X1 can be simplified to true</pre>
+ * This rewrite rule replaces subformulas of the form _X1&_X2
+ * with the counterpart of _X2 whenever it can be determined by simple
+ * means that the counterpart of _X1 in the actual occurrence is
+ * equivalent to true.
  */
 public class ConditionalTRS {
 
     /**
      * the syntax of expressions used for parsing.
      */
-    private ClassicalLogic syntax;
+    private final ClassicalLogic syntax;
+
+    private final Substitution rewrite;
 
     public static void main(String args[]) throws Exception {
         (new ConditionalTRS()).run();
@@ -28,6 +35,17 @@ public class ConditionalTRS {
 
     public ConditionalTRS() {
         syntax = new ClassicalLogic();
+        // rewrite rules
+        rewrite = Substitutions.getInstance(Arrays.asList(new Object[] {
+            Substitutions.createSingleSidedMatcher(syntax.createExpression("_X1&_X2"), 
+						   syntax.createExpression("_X2"), 
+						   new Simplifyable(syntax.createExpression("_X1"))), 
+ 	    Substitutions.createSingleSidedMatcher(syntax.createExpression("_X1|false"), 
+ 						   syntax.createExpression("_X1")), 
+ 	    Substitutions.createSingleSidedMatcher(syntax.createExpression("false|_X1"), 
+ 						   syntax.createExpression("_X1"))
+	}));
+
     }
 
     public void run() throws Exception {
@@ -38,23 +56,14 @@ public class ConditionalTRS {
         System.out.println("Original expression:\t" + s);
         System.out.println("Parsed expression:\t" + expression);
 
-        // rewrite rules
-        Substitution rewrite = Substitutions.getInstance(Arrays.asList(new Object[] {
-            Substitutions.createSingleSidedMatcher(syntax.createExpression("_X1&_X2"), 
-						   syntax.createExpression("_X2"), 
-						   new Simplifyable(syntax.createExpression("_X1"))), 
- 	    Substitutions.createSingleSidedMatcher(syntax.createExpression("_X1|false"), 
- 						   syntax.createExpression("_X1")), 
- 	    Substitutions.createSingleSidedMatcher(syntax.createExpression("false|_X1"), 
- 						   syntax.createExpression("_X1"))
-	}));
-
+	// apply the rewrite system step-wise
         System.out.println("applying " + rewrite);
         System.out.println("is");
         System.out.println("rewrite(A) = " + rewrite.apply(expression));
         System.out.println("rewrite^2(A) = " + rewrite.apply(rewrite.apply(expression)));
         System.out.println("rewrite^3(A) = " + rewrite.apply(rewrite.apply(rewrite.apply(expression))));
 
+	// apply the rewrite system until completion
         Object obj = Functionals.fixedPoint(rewrite, expression);
         System.out.println("TRS:\t\t\tA->" + obj);
     }
@@ -80,8 +89,13 @@ public class ConditionalTRS {
 	    try {
 		// the partial single sided matcher
 		Substitution mu = (Substitution)obj;
-		// the counterpart of "check" in the actual occurrence of the pattern
+		// the counterpart of check in the actual occurrence of the pattern
 		Expression occurrence = (Expression)mu.apply(check);
+		// check whether the occurrence of check is true.  Of
+		// course, it would make even more sense to check
+		// whether it is of the form ~_X3|X3 rather than just
+		// checking for a and b. But this should be sufficient
+		// for a simple demonstration.
 		return occurrence.equals(syntax.createExpression("true")) 
 		    || occurrence.equals(syntax.createExpression("~a|a")) 
 		    || occurrence.equals(syntax.createExpression("~b|b"));
